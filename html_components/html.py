766189e1e562c585aa112830eba5aefd094346b9
Akarsh Jain <akarsh.1995.02@gmail.com>
2019-11-02 13:37:39 +0530

    Html class write html method added.

--- html_components/html.py
+++ html_components/html.py
@@ -7,25 +7,31 @@ from .tag import TagEditor
 
 class Html:
 
-    def __init__(self, html_file_path):
-        self.dir_path = Path(html_file_path)
+    def __init__(self, html_file_path: Path):
+        self.html_file_path = html_file_path
         self.html_content = self._read_html()
         self.tag_editor = TagEditor(self.html_content)
-        self.head = self.get_headers()
-        self.body = self.get_body()
+        self.head = self._get_headers()
+        self.body = self._get_body()
 
     def _read_html(self) -> str:
         """
 
         :return: html string
         """
-        return self.dir_path.read_text()
+        return self.html_file_path.read_text()
 
-    def get_headers(self) -> Header:
+    def _get_headers(self) -> Header:
         return Header(self.tag_editor.get_main_tag_content('head', include_tags=False))
 
-    def get_body(self) -> Body:
+    def _get_body(self) -> Body:
         return Body(self.tag_editor.get_main_tag_content('body', include_tags=False))
+
+    def get_modified_html(self):
+        return self.head.get_header_block() + self.body.get_block_content()
+
+    def write_html(self, dir_path: Path):
+        dir_path.joinpath(self.html_file_path.name).write_text(self.get_modified_html())
 
     def __repr__(self):
         return self.head.__repr__() + self.body.__repr__()
